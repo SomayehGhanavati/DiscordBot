041bddcefe6da2fbbab7cf511c017a44d087826c
Somayeh.Ghanavati <s.ghanavati94@gmail.com>
2023-05-11 13:12:46 +0330

    Discord

---
+++ readme.md
@@ -0,0 +1,6 @@
+#npm init -y
+#npm i discord.js
+#npm install -g nodemon
+#npm dotenv ##https://www.youtube.com/watch?v=KZ3tIGHU314
+
+##https://discord.com/channels/<Guild_ID>/<ChannelID>
---
+++ src/index.js
@@ -0,0 +1,22 @@
+const { Client, IntentsBitField } = require('discord.js')
+require('dotenv').config()
+const client = new Client({
+  intents: [
+    IntentsBitField.Flags.Guilds,
+    IntentsBitField.Flags.GuildMembers,
+    IntentsBitField.Flags.GuildMessages,
+    IntentsBitField.Flags.MessageContent,
+  ],
+})
+client.on('ready', (c) => {
+  console.log(`${c.user.tag} is online`)
+})
+
+client.on('messageCreate', (message) => {
+  if (message.author.bot) return
+  if (message.content === 'hello') {
+    message.reply('hello!')
+  }
+})
+
+client.login(process.env.TOKEN)
